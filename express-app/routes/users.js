11c0d6782bf393c6bd102e5a6603a016b41e6b59
egorvts <egor.vetosh2310@gmail.com>
2026-03-24 18:30:29 +0500

    3

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,9 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+router.get("/", function (req, res, next) {
+    res.send({
+        items: [
+            {
+                id: 1,
+                name: "Valeria",
+            },
+            {
+                id: 2,
+                name: "Egor",
+            },
+        ],
+    });
 });
 
 module.exports = router;
