ecaf4c88ed4a2c59d65f7cbbe0f37ee2464cf915
Валерия Москвина <v.y.moskvina@gmail.com>
2026-03-24 18:42:40 +0500

    4-5

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,20 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
+let users = [
+    {
+        id: 1,
+        name: "Valeria",
+    },
+    {
+        id: 2,
+        name: "Egor",
+    },
+];
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
     res.send({
-        items: [
-            {
-                id: 1,
-                name: "Valeria",
-            },
-            {
-                id: 2,
-                name: "Egor",
-            },
-        ],
+        items: users,
     });
 });
+
+router.post("/", (req, res) => {
+    const newUser = req.body;
+    users.push(newUser);
+    res.status(201).json(newUser);
+})
 
 module.exports = router;
