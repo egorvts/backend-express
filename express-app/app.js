5944c08ddf4f7b4836d5fb783928ae53f5d467c4
Валерия Москвина <v.y.moskvina@gmail.com>
2026-03-24 18:49:16 +0500

    7

--- express-app/app.js
+++ express-app/app.js
@@ -13,6 +13,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+    if (req.query["auth"] !== "true") {
+        return res.sendStatus(401)
+    }
+    return next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
